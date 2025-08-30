45d59efd9949e3e7eefb456c2e7954747db75b08
Mahmoud Maher <tea0mah2009@gmail.com>
2025-08-30 21:56:59 +0300

    Update header-script.js

--- js/header-script.js
+++ js/header-script.js
@@ -2,10 +2,10 @@ let icon = document.querySelector(".icon_menu_and_close");
 function toggleMenu(menuid) {
     let menu = document.getElementById("menu_header");
     if (menu.style.top === "-400px") {
-        icon.style.backgroundImage = "url(../svg/icon_close.svg)";
+        icon.classList.add("close-icon");
         menu.style.top = "80px";
     } else {
-        icon.style.backgroundImage = "url(../svg/icon_menu.svg)";
+        icon.classList.remove("close-icon");
         menu.style.top = "-400px";
     }
 }
